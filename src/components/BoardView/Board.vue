b02c52e32b120547aadd1154ecb4bda3e627f3ce
NickolasBoyer <rut.willems@gmail.com>
2017-04-05 23:54:20 +0200

    Add base structure user card

--- src/components/BoardView/Board.vue
+++ src/components/BoardView/Board.vue
@@ -1,14 +1,38 @@
 <template lang="jade">
   .main
+    user-card(v-for="user in users", :user="user")
 </template>
 
 <script>
+import UserCard from '@/components/BoardView/Board/UserCard'
+
 export default {
-  name: 'board'
+  name: 'board',
+  components: {
+    UserCard
+  },
+  data: () => {
+    return {
+      users: [
+        {
+          name: "Rutger",
+          img: "https://graph.facebook.com/100001764027428/picture?width=999&height=999",
+          title: "Bierkoning 2017"
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+.main {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  top: 40px;
+  background-color: #F6F6F6;
+}
 
 </style>
---
+++ src/components/BoardView/Board/UserCard.vue
@@ -0,0 +1,49 @@
+<template lang="jade">
+  .card
+    img.user-image(:src="user.img")
+    .name {{user.name}}
+    .title {{user.title}}
+    .progress-bar
+</template>
+
+<script>
+export default {
+  name: 'user-card',
+  props: ['user']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$prim-white: white;
+
+$card-width: 10em;
+$card-height: $card-width*1.3;
+
+.card {
+    height: $card-height;
+    width: $card-width;
+    margin: 20px;
+    background-color: $prim-white;
+    border-radius: 5px;
+    overflow: hidden;
+    text-align: left;
+}
+
+.user-image {
+  width: $card-width;
+}
+
+.name, .title {
+  margin-left: 10px;
+}
+
+.name {
+  font-size: 1em;
+}
+
+.title {
+  font-size: 0.5em;
+}
+
+</style>
