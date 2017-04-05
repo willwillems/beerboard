951eea8aaebdffd75ac0754eebb946b7729abd3d
NickolasBoyer <rut.willems@gmail.com>
2017-04-05 12:54:00 +0200

    Set up basic look of menu bar

--- src/components/BoardView/MenuBar.vue
+++ src/components/BoardView/MenuBar.vue
@@ -1,5 +1,9 @@
 <template lang="jade">
-  .main test
+  .bar
+    .menu-element#logo BB
+    .menu-element#time-date 15:33
+    .menu-element#settings
+      i.material-icons settings
 </template>
 
 <script>
@@ -10,5 +14,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+$prim-white: white;
+
+$bar-height: 40px;
+
+.bar {
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  height: $bar-height;
+  background-color: $prim-white;
+  border-bottom-width: 1px;
+  border-bottom-color: lightgray;
+  border-bottom-style: solid;
+}
+
+.menu-element {
+  display: inline-block;
+  line-height: $bar-height;
+  font-size: 2em;
+  margin: 0px 15px;
+  opacity: 0.71;
+}
+
+#logo {
+  float: left;
+  opacity: 0.9;
+  font-weight: 600;
+}
+
+#settings {
+  float: right;
+  // I really don't like that this is neccecairy, is there a way to 
+  // get around this, it makes the CSS seem more complex than it is.
+  .material-icons {
+    font-size: 1em;
+    line-height: $bar-height;
+  }
+}
 
 </style>
