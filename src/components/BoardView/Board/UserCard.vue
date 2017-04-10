2b25759f159ba5d271388e93b5e773c0a7241611
NickolasBoyer <rut.willems@gmail.com>
2017-04-10 23:05:58 +0200

    Add backgroudn circle for beer count on card

--- src/components/BoardView/Board/UserCard.vue
+++ src/components/BoardView/Board/UserCard.vue
@@ -1,5 +1,6 @@
 <template lang="jade">
   .card(draggable="true" ,:id="user.uid")
+    .beer-count-circle
     .user-image(:style="'background-image: url(' + user.img + ')'")
     .name {{user.name}}
     .title {{user.title}}
@@ -21,6 +22,7 @@ $card-width: 10em;
 $card-height: $card-width*1.3;
 
 .card {
+    position: relative;
     height: $card-height;
     width: $card-width;
     margin: 20px;
@@ -34,6 +36,16 @@ $card-height: $card-width*1.3;
       transform: translateY(-3px); // 3px up
       box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
     }
+}
+
+.beer-count-circle {
+  width: 0.6*$card-width;
+  height: 0.6*$card-width;
+  background-color: white;
+  border-radius: 0.3*$card-width;
+  top:-0.3*$card-width;
+  left:-0.3*$card-width;
+  position: absolute;
 }
 
 .user-image {
