3e70a4ccc48c10643a5b02c37c7bd9cbe6110d67
NickolasBoyer <rut.willems@gmail.com>
2017-04-15 15:04:01 +0200

    Implement base circulair list in cart

--- src/components/BoardView/Cart.vue
+++ src/components/BoardView/Cart.vue
@@ -1,5 +1,7 @@
 <template lang="jade">
   .main(@drop="drop", @dragover="dragover", :class="classObject")
+    .menulist
+      .menuitem(v-for="item in items", :style="'background-image: url(' + item.img + ')'") 
 </template>
 
 <script>
@@ -9,7 +11,15 @@ export default {
     return {
       classObject: {
         'drag-over': false
-      }
+      },
+      items: [
+        {
+          name: "Rutger",
+          uid: 32467891,
+          img: "https://graph.facebook.com/100001764027428/picture?width=999&height=999",
+          title: "Bierkoning 2017"
+        }
+      ]
     }
   },
   methods: { // No arrow functions here for thas gets messed up, naturally
@@ -27,6 +37,56 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 $cart-radius: 150px;
+
+/// Mixin to place items on a circle
+/// @author Hugo Giraudel
+/// @author Ana Tudor
+/// @param {Integer} $item-count - Number of items on the circle
+/// @param {Length} $circle-size - Large circle size
+/// @param {Length} $item-size - Single item size
+@mixin on-circle($item-count, $circle-size, $item-size) {
+  position: relative;
+  width:  $circle-size;
+  height: $circle-size;
+  padding: 0;
+  border-radius: 50%; 
+  list-style: none;       
+  
+  > * {
+    display: block;
+    position: absolute;
+    top:  50%; 
+    left: 50%;
+    width:  $item-size;
+    height: $item-size;
+    margin: -($item-size / 2);
+  
+    $angle: (360 / $item-count);
+    $rot: 200; // modified so it starts at 10 a' clock
+
+    @for $i from 1 through $item-count {
+      &:nth-of-type(#{$i}) {
+        transform: 
+          rotate($rot * 1deg) 
+          translate($circle-size / 2) 
+          rotate($rot * -1deg);
+      }
+
+      $rot: $rot + $angle;
+    }
+  }
+}
+
+.menulist {
+    @include on-circle($item-count: 8, $circle-size: 2*$cart-radius, $item-size: 80px); 
+
+    .menuitem {
+      display: block; 
+      max-width: 100%; 
+      border-radius: 50%;
+      background-size: cover;
+    }
+}
 
 .main {
   height: 2*$cart-radius;
