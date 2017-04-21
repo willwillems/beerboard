0937c60edbbd9a3495e65457d96678a66146367d
NickolasBoyer <rut.willems@gmail.com>
2017-04-21 13:06:28 +0200

    Move from transaction to set for history logging The transaction shit messed with the validate rule and it isn't really neccecairy here

--- src/firebaseFunctions.js
+++ src/firebaseFunctions.js
@@ -7,22 +7,12 @@ export const addBeerToUser = function ({uid, beersInCart}) {
     user.beers += beersInCart
     return user
   })
-  // TODO catch error if history does not exist
-  db.ref(`history/${uid}`).transaction(function (history) {
-    // Firebase usually returns a null value while retrieving a
-    // key for the first time but while saving it checks if
-    // the new value is similar to older value or not.
-    // If not, firebase will run the whole process again, and
-    // this time the correct value is returned by the server
-    if (history === null) {
-      return {}
-    }
-    history[(new Date()).getTime()] = {
-      time: (new Date()).getTime(),
-      beers: beersInCart,
-      metaData: false
-    }
-    return history
+
+  const timeOfTransaction = (new Date()).getTime()
+  db.ref(`history/${uid}/${timeOfTransaction}`).set({
+    time: timeOfTransaction,
+    beers: beersInCart,
+    metaData: false
   })
 }
 
