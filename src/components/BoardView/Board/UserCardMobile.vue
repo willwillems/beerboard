7a9d28b2ef2868f3318b2ab3e51276a8ba6aada7
NickolasBoyer <rut.willems@gmail.com>
2017-05-31 20:25:29 +0200

    Remove tranform on hover for mobile

--- src/components/BoardView/Board/UserCardMobile.vue
+++ src/components/BoardView/Board/UserCardMobile.vue
@@ -71,11 +71,6 @@ $beer-count-radius-mobile: $card-width-mobile*0.18;
   background-size: cover;
   border-radius: 5px;
   box-shadow: 0 1px 2px rgba(0,0,0,0.1), 0 1px 3px rgba(0,0,0,0.13);
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-    &:hover {
-      transform: translateY(-3px); // 3px up
-      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
-    }
 }
 
 .name {
